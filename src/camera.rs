2424d9871ff1e9e30aee9b363bfcb560cd9c6fcf
Barlog <barlog@tanelorn.li>
2024-08-29 23:27:34 -0400

    update

---
+++ src/camera.rs
@@ -0,0 +1,25 @@
+use bevy::core_pipeline::bloom::BloomSettings;
+use bevy::core_pipeline::tonemapping::Tonemapping;
+use bevy::prelude::*;
+
+pub struct CameraPlugin;
+
+impl Plugin for CameraPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_camera);
+    }
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn((
+        Camera2dBundle {
+            camera: Camera {
+                hdr: true,
+                ..default()
+            },
+            tonemapping: Tonemapping::TonyMcMapface,
+            ..default()
+        },
+        BloomSettings::default(),
+    ));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+mod camera;
+
 use bevy::prelude::*;
+
+use camera::CameraPlugin;
 
 fn main() {
     App::new()
+        .insert_resource(ClearColor(Color::BLACK))
+        .insert_resource(AmbientLight {
+            color: Color::default(),
+            brightness: 750.0,
+        })
         .add_plugins(DefaultPlugins)
+        .add_plugins(CameraPlugin)
         .run();
 }
